34b88c68a3ffdc2b4c62ddbc7fa9ec2a825524f1
Daniel Chabr <danielchabr@gmail.com>
2013-05-23 15:21:08 +0200

    Code revised to work with deque, 780 lines, add commentaries and some more functionality

--- BigNum.cpp
+++ BigNum.cpp
@@ -1,9 +1,4 @@
 // BigNum.cpp
-#include <iostream>
-#include <cstring>
-#include <cstdlib>
-#include <vector>
-#include <climits>
 #include "BigNum.h"
 
 // testovat castecne preteceni, tzn. mene nez velikost datoveho typu ale vice nez pocet cifer ktere pouzivam
@@ -16,128 +11,76 @@ template <class Any>
 inline Any abs(const Any & x) { return x >= 0 ? x : -x; }
 
 
-int powerOfTen(int n) // implementovat pres enum
-{
+int powerOfTen(int n) { // implementovat pres enum
 	int x = 10;
-	if(n == 0)
-	{
+	if(n == 0) {
 		return 1;
 	}
-	for(int i = 1; i < n; i++)
-	{
+	for(int i = 1; i < n; i++) {
 		x *= 10;
 	}
 	return x;
 }
 
-BigNum::BigNum()
-{
-	number = NULL;
+BigNum::BigNum() {
 	exp = 0;
-	n_parts = 0;
-	sign = 1;
+	sign = PLUS_SIGN;
 }
 
-BigNum::BigNum(const char * str)
-{
+BigNum::BigNum(const string & str) {
 	setNum(str);
 }
 
-BigNum::BigNum ( const BigNum & x )
-{
-	this->n_parts = x.n_parts;
-	this->exp = x.exp;
-	this->sign = x.sign;
-	this->number = new int [n_parts];
-	for(int i = 0; i < n_parts; i++)
-	{
-		this->number[i] = x.number[i];
-	}
+BigNum::BigNum ( const BigNum & x ) {
+	exp = x.exp;
+	sign = x.sign;
+	number = x.number;
 }
 
-void BigNum::setNum(const char * str)
-{
-	number = NULL;
+void BigNum::setNum(const string & num) {
+	string str = num;
 	exp = 0;
-	n_parts = 0;
-	if ( str[0]=='-' ) //nastav pripadne znamenko na zacatku a preskoc ho
-	{
-		sign = -1;
-		str++;
+	if ( str[0]=='-' ) { //nastav pripadne znamenko na zacatku a preskoc ho
+		sign = MINUS_SIGN;
+		str.erase(0,1);
 	} 
-	else
-	{
-		if ( str[0] == '+' )
-			str++;
-		sign = +1;
+	else {
+		if ( str[0] == '+' ) {
+			str.erase(0,1);
+		}
+		sign = PLUS_SIGN;
 	}
-	int length = strlen(str);
-	int dot = 0;
-	for( int i = 0; i < length; i++) // najdi desetinnou tecku, pokud existuje
-	{
-		if (str[length - i - 1]=='.') // osetri dve tecky
-		{
-			if (dot)
-			{
-				throw Exception ("Number contains two dots, please try again:");
-			}
-			exp = - i;	
-			dot = 1;
+	unsigned int length = str.length();
+	//int padding = 0;
+	//				throw Exception ("Number contains two dots, please try again:");
+	unsigned int dot_pos;
+	int padding = 0;
+	if( str.find_last_of('.') != string::npos ) {
+		dot_pos = str.find_last_of('.');
+		str.erase(dot_pos,1);
+		length --;
+		exp = -(length - dot_pos);
+		padding = BASE - (-exp % BASE );
+		exp -= padding;
+		str.append(padding, '0');
+		length += padding;
+	}
+	// osetrit vstupy
+	for( int i = length - 1; i >= 0; i-- ) {
+		if( (length - i - 1)/BASE >= number.size() ) {
+			number.push_back( ctoi( str[i] ) * powerOfTen( (length - i - 1) % BASE ) );
+		} else {
+			number[(length - i - 1)/BASE] += ctoi( str[i] ) * powerOfTen( (length - i - 1) % BASE );
 		}
 	}
-	while ( str[0] == '0' || str[0] == '.') // vymaz pocatecni nuly
-	{
-		str++;
-		length--;
-	}
-	while ( str[ length - 1 ] == '0' ) // vymaz zaverecne nuly
-	{
-		exp++;
-		length--;
-	}
-	// zjisteni vyplne nulou a upraveni exponentu, tak aby desetinna tecka vychazela na okraj casti
-	int padding = exp % BASE;
-	if(padding<0)
-	  { 
-	  padding += BASE;
-	}
-	exp -= padding;
-	// zjisteni potrebneho poctu casti
-	n_parts = (length + padding - dot - 1) / BASE + 1;
-	// alokace potrebneho poctu casti 
-	number = new int [n_parts];
-	dot = 0;
-	for ( int i = 0; i < n_parts; i++ )
-	{
-		number[i] = 0;
-		for ( int j = i * BASE; j < BASE * ( i + 1) && j < length + padding - dot; j++ )
-		{
-			if(j >= padding)
-			{
-				if(str[length - 1 - j + padding] == '.')
-				{
-					dot = 1;
-				}
-				number[i] += powerOfTen(j - i * BASE) * ctoi(str[length - 1 - j + padding - dot]);
-			}
-		}
-	}
-}
+	Optimize();
+} 
 
 BigNum & BigNum::operator=(const BigNum & b) 
 {
-	if( number != NULL )
-	{
-		delete [] number;
-	}
-	this->n_parts = b.n_parts;
-	this->exp = b.exp;
-	this->sign = b.sign;
-	this->number = new int [n_parts];
-	for(int i = 0; i < n_parts; i++)
-	{
-		this->number[i] = b.number[i];
-	}
+	exp = b.exp;
+	sign = b.sign;
+	number = b.number;
 	return *this;
 }
 
@@ -153,371 +96,235 @@ int BigNum::checkBoundaries(const int & i, const BigNum & a, const int & min, co
 	}
 }
 
-BigNum BigNum::operator+(const BigNum & b) const 
-{
+BigNum BigNum::operator+(const BigNum & b) const {
 	BigNum result;
 	// zjisteni potrebneho rozsahu pro alokaci result
 	int min, max;
-	if( exp/BASE <= b.exp/BASE )
-	{
+	if( exp/BASE <= b.exp/BASE ) {
 		min = exp/BASE;
 		result.exp = exp;
-	}
-	else
-	{
+	} else {
 		min = b.exp/BASE;
 		result.exp = b.exp;
 	}
-	if( exp/BASE + n_parts > b.exp/BASE + b.n_parts )
-	{
-		max = exp/BASE + n_parts;
+	if( exp/BASE + number.size() > b.exp/BASE + b.number.size() ) {
+		max = exp/BASE + number.size();
+	} else {
+		max = b.exp/BASE + b.number.size();
 	}
-	else
-	{
-		max = b.exp/BASE + b.n_parts;
-	}
-	result.n_parts = max - min;
+	int n_parts = max - min;
 	int this_min = (exp - result.exp) / BASE;
-	int this_max = this_min + n_parts - 1;
+	int this_max = this_min + number.size() - 1;
 	int b_min = (b.exp - result.exp) / BASE;
-	int b_max = b_min + b.n_parts - 1;
+	int b_max = b_min + b.number.size() - 1;
 	bool flag = true;
-	if(sign == b.sign) // obe cisla jsou stejneho znamenka
-	{
+	if(sign == b.sign) { // obe cisla jsou stejneho znamenka
 		result.sign = sign;
-	}
-	else // rozdilneho znamenka -> zjistuji jake bude znamenko vysledku
-	{
-		int i = result.n_parts - 1;
-		while (flag && i >= 0)
-		{
+	} else { // rozdilneho znamenka -> zjistuji jake bude znamenko vysledku
+		int i = n_parts - 1;
+		while (flag && i >= 0) {
 			int x, y;
 			x = checkBoundaries(i, *this, this_min, this_max); // initialization of x and y, if i is out of boundaries, they are set to zero
 			y = checkBoundaries(i, b, b_min, b_max); // initialization of x and y, if i is out of boundaries, they are set to zero
-			/*if (i > this_max || i < this_min)
-			{
-				x = 0;
-			}
-			else 
-			{
-				x = number[i - this_min];
-			}
-			if (i > b_max || i < b_min)
-			{
-				y = 0;
-			}
-			else 
-			{
-				y = b.number[i - b_min];
-			}*/
-			if(abs(x) > abs(y))
-			{
+			if(abs(x) > abs(y)) {
 				result.sign = sign;
 				flag = false;
 			}
-			else if (abs(x) < abs(y))
-			{
+			else if (abs(x) < abs(y)) {
 				result.sign = b.sign;
 				flag = false;
 			}
-			else
-			{
-				result.n_parts--;
+			else {
+				//	result.n_parts--;
 			}
 			i--;
 		}	
 		if (flag == true)
 		{
-			result.n_parts = 0;
+			//	result.n_parts = 0;
 			result.exp = 0;
 		}
 	}
-	result.number = new int [result.n_parts](); // inicializace na nulu
+	//result.number = new int [result.n_parts](); // inicializace na nulu
 	int max_n = MAX + 1;
 	int carry = 0;
 	// pokud jsou vysledne casti odzadu nulove, je treba vysledek zmensit
-	for(int i = 0; i < result.n_parts; i++)
-	{
+	for(int i = 0; i < n_parts; i++) {
 		int x, y, help;
 		x = checkBoundaries(i, *this, this_min, this_max); // initialization of x and y, if i is out of boundaries, they are set to zero
 		y = checkBoundaries(i, b, b_min, b_max); // initialization of x and y, if i is out of boundaries, they are set to zero
-		x *= sign;
-		y *= b.sign;
+		if( this->sign == MINUS_SIGN) {
+			x *= -1;
+		}
+		if( b.sign == MINUS_SIGN) {
+			y *= -1;
+		}
 		// pokud jsou obe cisla kladna
-		if(x >= 0 && y >= 0)
-		{
+		if(x >= 0 && y >= 0) {
 			help = x - max_n + y + carry;
-			if(help>=0)
-			{
-				result.number[i] = help;
+			if(help>=0) {
+				result.number.push_back(help);
 				carry = 1;
-			}
-			else
-			{
-				result.number[i] = x + y + carry;
+			} else {
+				result.number.push_back( x + y + carry );
 				carry = 0;
 			}
-		}	
-		// pokud jsou obe zaporna
-		else if(x < 0 && y < 0)
-		{
+			// pokud jsou obe zaporna
+		} else if(x < 0 && y < 0) {
 			help = x + max_n + y + carry;
-			if(help<=0)
-			{
-				result.number[i] = help;
+			if(help<=0) {
+				result.number.push_back(help);
 				carry = -1;
 			}
-			else
-			{
-				result.number[i] = x + y + carry;
+			else {
+				result.number.push_back( x + y + carry );
 				carry = 0;
 			}
-		}
-		// pokud jsou rozdilneho znamenka
-		else 
-		{
-			result.number[i] = x + y + carry;
+			// pokud jsou rozdilneho znamenka
+		} else {
+			result.number.push_back( x + y + carry );
 			carry = 0;
-			if (result.number[i] < 0 && result.sign > 0)
-			{
+			if (result.number[i] < 0 && result.sign > 0) {
 				result.number[i] += max_n;
 				carry = -1;
-			}
-			else if (result.number[i] > 0 && result.sign < 0)
-			{
+			} else if (result.number[i] > 0 && result.sign < 0) {
 				result.number[i] -= max_n;
 				carry = 1;
 			}
 		}
 		// if the number is different sign than it should be
-		if ( ( result.sign == 1 ) && ( result.number[i] < 0 ) )
-		{
+		if ( ( result.sign == 1 ) && ( result.number[i] < 0 ) ) {
 			result.number[i] += max_n;
 			carry--;
-		}
-		else if ( ( result.sign == -1 ) && ( result.number[i] > 0 ) )
-		{
+		} else if ( ( result.sign == -1 ) && ( result.number[i] > 0 ) ) {
 			result.number[i] -= max_n;
 			carry++;
 		}
 		result.number[i] = abs( result.number[i] );
 	}
-	if ( carry )
-	{
-		int * bigger = new int [ ++result.n_parts ];
-		bigger[ result.n_parts - 1 ] = 1;
-		for ( int i = 0; i < result.n_parts - 1; i++ )
-		{
-			bigger[ i ] = result.number[ i ];
-		}
-		delete [] result.number;
-		result.number = bigger;
+	if ( carry ) {
+		result.number.push_back(carry);
 	}
-	//result.Optimize();
+	result.Optimize();
 	return result;
 }
-void BigNum::Optimize ( void ) // probably obsolete
-{
-	BigNum opt;
-	opt.n_parts = n_parts;
-	opt.exp = exp;
-	opt.sign = sign;
-	int i = 0;
-	while ( number[i] == 0 )
-	{
-		opt.n_parts --;
-		opt.exp -= BASE;
-		i++;
-	}
-	int j = n_parts - 1;
-	while ( number[j] == 0 )
-	{
-		opt.n_parts --;
-		opt.exp -= BASE;
-		j--;
-	}
-	if ( opt.n_parts )
-	{
-		opt.number = new int [ opt.n_parts ];
-		for ( int k = i; k <= j; k++ )
-		{
-			opt.number[ k - i ] = number[ k ];
+
+void BigNum::Optimize ( void ) { // probably obsolete
+	deque <int> ::iterator it;
+	if(number.size()) {
+		it = number.begin();
+		while( it < number.end() && *it == 0 ) {
+			number.pop_front();
+			exp += BASE;
+			it++;
+		}
+		it = number.end();
+		it--;
+		while( it >= number.begin() && *it == 0 ) {
+			number.pop_back();
+			it--;
 		}
 	}
-	*this = opt;
 }
-BigNum BigNum::operator-(const BigNum & b) const 
-{
+
+BigNum BigNum::operator-(const BigNum & b) const {
 	BigNum result;
 	BigNum c = b;
-	if ( c.sign == 1 )
-	{
-		c.sign = -1;
+	if ( c.sign == 1 ) {
+		c.sign = MINUS_SIGN;
 		result = *this + c;
 	}
-	else 
-	{
-		c.sign = 1;
+	else {
+		c.sign = PLUS_SIGN;
 		result = *this + c;
 	}
 	return result;
 }
 
-BigNum BigNum::operator * ( const BigNum & b ) const
-{
+BigNum BigNum::operator * ( const BigNum & b ) const {
 	BigNum result;
-	vector <int> vec;
-	int carry, /*a0, a1, b0, b1, zx, zy,*/ res;
-	for ( int i = 0; i < n_parts; i++ )
-	{
+	int carry, res;
+	for ( unsigned int i = 0; i < number.size(); i++ ) {
 		carry = 0;
-		for ( int j = 0; j < b.n_parts; j++ )
-		{
-			long long int x = number[i];
-			long long int y = b.number[j];
-			/*os << endl << x*y << endl;
-			a0 = number[i] & 0x0000ffff;
-			a1 = number[i] >> 16;  // change to macros
-			b0 = b.number[i] & 0x0000ffff;
-			b1 = b.number[j] >> 16;
-			zx = ( a1 * b0 );
-			zy = ( a0 * b1 );
-			res = ( a0 * b0 ) + ( zx << 16 ) + ( zy << 16 ) + carry;
-			carry = ( zx >> 16 ) + ( zy >> 16 ) + ( a1 * b1 );
-			os << endl << carry << endl;
-			int carry10 = 0;
-			for ( int k = 0; k < 32; k++ )
-			{
-				carry = carry << 1;
-				carry10 = carry10 << 1;
-				if ( carry < 0 )
-				{
-					carry10 ++;
-					carry /= 10;
-				}
-			}
-			os << endl << carry << endl;
-			res += carry % 1000000000;
-			carry10 += carry / 1000000000;
-			carry = carry10;
-			*/
+		for ( unsigned int j = 0; j < b.number.size(); j++ ) {
+			long long int x = number.at(i);
+			long long int y = b.number.at(j);
 			res = ((x*y) %1000000000) + carry;
 			carry = (x*y)/1000000000;
-			if ( ( i + j ) < (signed int) vec.size() )
-			{
-				vec.at( i + j ) += res;
-				/*if ( vec.at( i + j ) < 0 )
-				{
+			if ( ( i + j ) < result.number.size() ) {
+				result.number.at( i + j ) += res;
+				if ( result.number.at( i + j ) > MAX ) {
 					carry++;
-					vec.at( i + j ) -= max_n;
-				}*/
-				if ( vec.at( i + j ) > MAX )
-				{
-					carry++;
-					vec.at( i + j ) -= MAX +1;
+					result.number.at( i + j ) -= MAX +1;
 				}
-			}
-			else
-			{
-				vec.push_back(res);
+			} else {
+				result.number.push_back(res);
 			}
 		}
-		if ( carry )
-		{
-			vec.push_back( carry );
+		if ( carry ) {
+			result.number.push_back( carry );
 		}
 	}
-
 	result.exp = exp + b.exp;
-	result.n_parts = vec.size();
-	result.sign = sign * b.sign;
-	bool resize = false;
-	if ( result.n_parts > 1 && vec.at(0) == 0 )
-	{
-		result.n_parts --;
-		resize = true;
-		result.exp += BASE;
-	}
-	result.number = new int [result.n_parts];
-	vector <int> ::iterator it;
-	int i = 0;
-	for ( it = vec.begin(); it != vec.end(); it++ )
-	{
-		if(resize)
-		{
-			it++;
-			resize = false;
-		}
-		result.number[i] = *it;
-		i++;
+	if( sign == b.sign ) {
+		result.sign = PLUS_SIGN;
+	} else {
+		result.sign = MINUS_SIGN;
 	}
 
+	result.Optimize();
 	return result;
 }
 
-ostream & operator << ( ostream & os, const BigNum & n )
-{
-	if(n.n_parts == 0)
-	{
+ostream & operator << ( ostream & os, const BigNum & n ) {
+	if(n.number.size() == 0) {
 		os << "0";
+		os << endl;
+		return os;
 	}
-	if(n.sign == -1)
-	{
+	if(n.sign == MINUS_SIGN) {
 		os << '-';
 	}
-	int dotpart = -n.exp / BASE;
-	bool dotprinted = false;
-	if(n.exp<0 && dotpart >= n.n_parts)
-	{
+	int dot_part = -n.exp / BASE;
+	bool dot_printed = false;
+	if( n.exp < 0 && dot_part >= (signed) n.number.size() ) {
 		os << "0.";
-		dotprinted = true;
+		dot_printed = true;
 	}
-	for(int i = 0; i < dotpart - n.n_parts; i++)
-	{
-		for(int j = 0; j < BASE; j++)
-		{
+	for(int i = 0; i < dot_part - (signed) n.number.size(); i++) {
+		for(int j = 0; j < BASE; j++) {
 			os << "0";
 		}
 	}
 	os.fill('0');
 	bool first = true;
-	for(int i = n.n_parts-1; i >= 0; i--)
-	{
-		if(i == dotpart - 1 && !dotprinted) // vypis casti s desetinnou teckou
-		{
+	for(int i = n.number.size()-1; i >= 0; i--) {
+		if(i == dot_part - 1 && !dot_printed) { // vypis casti s desetinnou teckou
 			os << '.';
-			dotprinted = true;
+			dot_printed = true;
 		}
-		if(dotprinted && i==0)
-		{
+		if(dot_printed && i==0) {
 			int printn = n.number[i];
 			int j = 0;
-			while(printn % 10 == 0 && printn != 0)
-			{
+			while(printn % 10 == 0 && printn != 0) {
 				printn = printn / 10; // nema zde byt exp ++ nebo neco takovyho?
 				j++;
 			}
-			if(dotprinted)
-			{
+			if(dot_printed) {
 				os.width(BASE - j);
 			}
 			os << printn;
-		}
-		else
-		{
+		} else {
 			if(!first)
 			{
 				os.width(BASE);
 			}
 			os << n.number[i];
 			first = false;
-			//dotprinted = true;
 		}
 	}
-	if(n.exp > 0)
-	{
-		for(int i = 0; i < n.exp; i += BASE)
-		{
+	if(n.exp > 0) {
+		for(int i = 0; i < n.exp; i += BASE) {
 			os.width(BASE);
 			os << "0";
 		}
@@ -525,12 +332,3 @@ ostream & operator << ( ostream & os, const BigNum & n )
 	os << endl;
 	return os;
 }
-
-BigNum::~BigNum()
-{
-	if ( number != NULL )
-	{
-		delete [] number;
-	}
-}
-
